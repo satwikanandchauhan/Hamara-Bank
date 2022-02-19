3736773f8d0b37de8f20273cedf596a86ca32ae9
Satwik Anand Chauhan <93284505+satwikanandchauhan@users.noreply.github.com>
2022-02-19 21:30:22 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>HAMARA BANK</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
+<header>
+  <?php
+  include 'navbar.php';
+  ?></header>
+  
+
+ <h1 class="text-left font-weight-bold mt-5 ml-5 p-5  ">Welcome to Hamara Bank! </h1>
+
+</body>
+</html>
